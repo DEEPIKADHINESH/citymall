0f887ec6962afd62e67a80077edcd9dbf0ce54e3
DEEPIKADHINESH <77334795+DEEPIKADHINESH@users.noreply.github.com>
2021-12-17 18:30:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+app.use(express.json())
+const mongoose=require("mongoose")
+const user=require("./user-router")
+const login=require("./auth-router")
+mongoose.connect("mongodb://localhost/citymallproject")
+.then(()=>console.log("connected to db"))
+.catch((err)=>console.log("someerror occured",err))
+app.use("/user/create",user)
+app.use("/user/login",login)
+const port=5000;
+app.listen(port,()=>{
+  console.log(`server started at port${port}`)
+})
