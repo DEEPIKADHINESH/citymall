b4f4f9308be756fcb4c4c87aa27d247a18d2b288
DEEPIKADHINESH <77334795+DEEPIKADHINESH@users.noreply.github.com>
2021-12-19 19:33:49 +0530

    auth-token

--- auth-router.js
+++ auth-router.js
@@ -4,14 +4,16 @@ const {User}=require("./user-model")
 const jwt=require("jsonwebtoken")
 const Joi=require("joi")
 const bcrypt=require("bcrypt")
+const auth=require("./auth-token-middleware")
+
 route.post("/",async(req,res)=>{
     const {error}=validate(req.body)
     if(error) return res.status(404).send(error.details[0].message)
     let user=await User.findOne({email:req.body.email})
     if(!user) return res.status(400).send("invalid username")
     const validPassword=await bcrypt.compare(req.body.password,user.password)
-    if(!validPassword) return res.status(400).send("paswor is invalid")
-    const token=jwt.sign({_id:user._id},"jsonwebtoken")
+    if(!validPassword) return res.status(400).send("password is invalid")
+    const token=user.generateAuthToken()
     res.send(token)
 })
 function validate(user){
